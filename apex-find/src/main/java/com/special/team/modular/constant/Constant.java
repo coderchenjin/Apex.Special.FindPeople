5c583d4db55d5ed69859f6d0d1606f6ce12c19b6
ShiFa.Zhang <1045960348@qq.com>
2019-10-09 14:35:18 +0800

    no message

--- apex-find/src/main/java/com/special/team/modular/constant/Constant.java
+++ apex-find/src/main/java/com/special/team/modular/constant/Constant.java
@@ -5,21 +5,21 @@ package com.special.team.modular.constant;
  */
 public class Constant {
 
-    public static final String DOMAIN = "https://139.199.193.14";
+   // public static final String DOMAIN = "";
 
-    public static final String APP_ID = "wx7cfc1b0a87b17035";
+    public static final String APP_ID = "wxea808207371e2f61";
 
-    public static final String APP_SECRET = "9040b4e7826646d59df36b333924fd0a";
+    public static final String APP_SECRET = "dd897c20ff7401d3b545598e5cb92baf";
 
-    public static final String APP_KEY = "qwertyuiopasdfghjklzxcvbnmA12345";
-
-    public static final String MCH_ID = "1502023561";  //商户号
-
-    public static final String URL_UNIFIED_ORDER = "https://api.mch.weixin.qq.com/pay/unifiedorder";
-
-    public static final String URL_NOTIFY = Constant.DOMAIN + "/signature-partal/views/payInfo.jsp";
-
-    public static final String TIME_FORMAT = "yyyyMMddHHmmss";
+//    public static final String APP_KEY = "qwertyuiopasdfghjklzxcvbnmA12345";
+//
+//    public static final String MCH_ID = "1502023561";  //商户号
+//
+//    public static final String URL_UNIFIED_ORDER = "https://api.mch.weixin.qq.com/pay/unifiedorder";
+//
+//    public static final String URL_NOTIFY = Constant.DOMAIN + "/signature-partal/views/payInfo.jsp";
+//
+//    public static final String TIME_FORMAT = "yyyyMMddHHmmss";
 
     public static final int TIME_EXPIRE = 2;  //单位是day
 	
